1abe7fa1984a9ffa2de4e45f9a62fe53ca9993a0
fro_ozen <fro_ozen@gmx.de>
2014-01-09 21:34:44 +0100

    Added Canvas to GameFrame

--- Dungeon Game/src/dungeongame/GameFrame.java
+++ Dungeon Game/src/dungeongame/GameFrame.java
@@ -1,9 +1,12 @@
 package dungeongame;
 
+import java.awt.Canvas;
+
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame{
 	private static final long serialVersionUID = 1L;
+	private Canvas gameCanvas;
 	
 	public GameFrame(){
 		setSize(800, 600);
@@ -12,6 +15,10 @@ public class GameFrame extends JFrame{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(null);
 		
+		gameCanvas = new Canvas();
+		gameCanvas.setBounds(0, 0, 800, 600);
+		
+		add(gameCanvas);
 		setVisible(true);
 	}
 	
