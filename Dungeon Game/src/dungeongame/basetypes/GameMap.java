dac0bb9c819229509bf740da3f1e8cd4db7dcf74
fro_ozen <fro_ozen@gmx.de>
2014-01-18 11:25:50 +0100

    Maps now draw themself

--- Dungeon Game/src/dungeongame/basetypes/GameMap.java
+++ Dungeon Game/src/dungeongame/basetypes/GameMap.java
@@ -1,7 +1,9 @@
 package dungeongame.basetypes;
 
+import java.awt.Graphics;
 import java.util.ArrayList;
 
+import dungeongame.RessourceManager;
 import dungeongame.entitys.BaseEntity;
 
 public class GameMap {
@@ -20,6 +22,27 @@ public class GameMap {
 			for(int y = 0; y<height; y++){
 				tiles[x][y] = 1;
 			}
+		}
+	}
+	
+	public void drawMe(Graphics g){
+		for(int x = 0; x < tiles.length; x++){
+			for(int y = 0; y < tiles[x].length; y++){
+				g.drawImage(RessourceManager.getTile(tiles[x][y]), x * RessourceManager.tileSize, y * RessourceManager.tileSize, null);
+			}
+		}
+		
+		//Draw debug lines
+		for(int x = 0; x < tiles.length;x++){
+			g.drawLine(x * RessourceManager.tileSize, 0, x * RessourceManager.tileSize, tiles[x].length * RessourceManager.tileSize);
+		}
+		for(int y = 0; y < tiles[0].length;y++){
+			g.drawLine(0, y * RessourceManager.tileSize, tiles.length * RessourceManager.tileSize, y * RessourceManager.tileSize);
+		}
+		
+		
+		for(BaseEntity entity:entitys){
+			entity.drawMe(g);
 		}
 	}
 }
--- Dungeon Game/src/dungeongame/states/DungeonMapState.java
+++ Dungeon Game/src/dungeongame/states/DungeonMapState.java
@@ -3,7 +3,6 @@ package dungeongame.states;
 import java.awt.Graphics;
 import java.awt.Point;
 
-import dungeongame.RessourceManager;
 import dungeongame.basetypes.GameMap;
 import dungeongame.entitys.BaseEntity;
 import dungeongame.entitys.Player;
@@ -17,24 +16,7 @@ public class DungeonMapState extends BaseState{
 	}
 	
 	public void drawMe(Graphics g) {
-		for(int x = 0; x < activeMap.tiles.length; x++){
-			for(int y = 0; y < activeMap.tiles[x].length; y++){
-				g.drawImage(RessourceManager.getTile(activeMap.tiles[x][y]), x * RessourceManager.tileSize, y * RessourceManager.tileSize, null);
-			}
-		}
-		
-		//Draw debug lines
-		for(int x = 0; x < activeMap.tiles.length;x++){
-			g.drawLine(x * RessourceManager.tileSize, 0, x * RessourceManager.tileSize, activeMap.tiles[x].length * RessourceManager.tileSize);
-		}
-		for(int y = 0; y < activeMap.tiles[0].length;y++){
-			g.drawLine(0, y * RessourceManager.tileSize, activeMap.tiles.length * RessourceManager.tileSize, y * RessourceManager.tileSize);
-		}
-		
-		
-		for(BaseEntity entity:activeMap.entitys){
-			entity.drawMe(g);
-		}
+		activeMap.drawMe(g);
 	}
 
 	public void computeNextFrame() {
