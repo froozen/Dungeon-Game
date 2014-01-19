4469ecefd3a36393945f254ffdf92568e8c6d0ec
fro_ozen <fro_ozen@gmx.de>
2014-01-19 10:58:31 +0100

    Cleaned up the code

--- Dungeon Game/src/dungeongame/entitys/Player.java
+++ Dungeon Game/src/dungeongame/entitys/Player.java
@@ -7,21 +7,21 @@ import dungeongame.RessourceManager;
 import dungeongame.basetypes.GameMap;
 
 public class Player extends BaseEntity{
-	
-	
+
+
 	public Player(Point position, GameMap locationMap){
 		this.locationMap = locationMap;
 		this.position = position;
 		this.x = position.x;
 		this.y = position.y;
-				
+
 		direction = Direction.DOWN;
 		cyclePhase = 0;
 		moving = false;
-		
+
 		spriteLocation = "sprites.player";
 		sprite = RessourceManager.getCharacterSprite(spriteLocation, direction, cyclePhase);
-		
+
 		locationMap.occupied[position.x][position.y] = true;
 	}
 
@@ -31,10 +31,6 @@ public class Player extends BaseEntity{
 			else if(GameInput.wasKeyDown("A")) moveDirection(Direction.LEFT);
 			else if(GameInput.wasKeyDown("D")) moveDirection(Direction.RIGHT);
 			else if(GameInput.wasKeyDown("S")) moveDirection(Direction.DOWN);
-			
-			//TODO create moveDirection() method
-			
-			if(moving)System.out.println("target: x=" + position.x + " y=" + position.y);
 		}
 	}
 }
