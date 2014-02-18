94670c3d42409b29b96e396cc17e9d13aa1d8018
fro_ozen <fro_ozen@gmx.de>
2014-02-18 17:05:08 +0100

    Created Inventory

---
+++ Dungeon Game/src/dungeongame/Inventory.java
@@ -0,0 +1,13 @@
+package dungeongame;
+
+import java.util.ArrayList;
+
+import dungeongame.basetypes.items.BaseItem;
+
+public class Inventory {
+	public static ArrayList<BaseItem> items;
+	
+	static{
+		items = new ArrayList<BaseItem>();
+	}
+}
